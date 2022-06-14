84017003a719c07e7d95931768d4fa46a077154b
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-06-14 11:52:37 -0300

    Integra difusão e decaimento

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import Canvas from './components/Canvas/Canvas';
 import './App.css';
 
 // Tamanho da matriz de plano
-const LINHAS = 11;
-const COLUNAS = 11;
+const LINHAS = 5;
+const COLUNAS = 5;
 
 export default function App() {
 
   let plano = [];
+  let planoFuturo = [];
   let t0 = null; // Tempo inicial
 
   // Preenche o plano com valores aleatórios
   for (let l = 0; l < LINHAS; l++) {
     plano[l] = [];
+    planoFuturo[l] = [];
     for (let c = 0; c < COLUNAS; c++) {
       plano[l][c] = Math.random() * 0; // Número aleatório entre 0 e 9.99...
     }
   }
 
   //Define um espaço no plano como valor máximo
-  plano[Math.floor(LINHAS / 2)][Math.floor(COLUNAS / 2)] = 10;
+  plano[Math.floor(LINHAS / 2)][Math.floor(COLUNAS / 2)] = 1000;
 
   // Desenha na tela
   const draw = (ctx, t) => {
@@ -30,28 +32,25 @@ export default function App() {
     // Limpa desenho anterior
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    // Difusãpo da concetração
-    difusao(plano, dt);
-
-    // Decaimento de intensidade
-    //decaimento(plano, dt);
-
     // Desenha na tela conforme os valores do plano
     for (let l = 0; l < LINHAS; l++) {
       for (let c = 0; c < COLUNAS; c++) {
         ctx.fillStyle = `hsl(${(10 - plano[l][c]) * 10}deg, 100%, 50%)`;
         ctx.fillRect(c * 10, l * 10, 10, 10);
-        ctx.fillStyle = "black";
+        //ctx.fillStyle = "black";
         //ctx.fillText(plano[l][c].toFixed(3), l * 10, c * 10 + 10);
       }
     }
     //ctx.fillText(dt * 1, 10, 10);
 
+    // Difusão da concetração
+    difusao(plano, planoFuturo, dt);
+
+    atualizaPlano(plano, planoFuturo);
+
     // Define tempo inicial da próxima animação como o tempo atual
     t0 = t;
   };
-
-  console.table(plano);
 
   return (
     <div className="app">
@@ -62,41 +61,47 @@ export default function App() {
   );
 }
 
-function difusao(plano, dt) {
+function calculaAcrescimoIntensidade(plano, l, c) {
+  const OC = 0.2;
+  const DC = 0.05;
+  let intensidadeRedor = 0;
 
-  const fatorDifusao = 0.1;
-  let qtdDifusoes = 0;
+  if (l - 1 > 0) intensidadeRedor += plano[l - 1][c] * OC;
+  if (l + 1 < LINHAS) intensidadeRedor += plano[l + 1][c] * OC;
+  if (c - 1 > 0) intensidadeRedor += plano[l][c - 1] * OC;
+  if (c + 1 < COLUNAS) intensidadeRedor += plano[l][c + 1] * OC;
+  if (l - 1 > 0 && c - 1 > 0) intensidadeRedor += plano[l - 1][c - 1] * DC;
+  if (l - 1 > 0 && c + 1 < COLUNAS) intensidadeRedor += plano[l - 1][c + 1] * DC;
+  if (l + 1 < LINHAS && c - 1 > 0) intensidadeRedor += plano[l + 1][c - 1] * DC;
+  if (l + 1 < LINHAS && c + 1 < COLUNAS) intensidadeRedor += plano[l + 1][c + 1] * DC;
+  intensidadeRedor += plano[l][c] * -1;
 
-  for (let l = 0; l < LINHAS; l++) {
-    for (let c = 0; c < COLUNAS; c++) {
-      const espaco = plano[l][c];
-
-      if (l - 1 >= 0) { plano[l - 1][c] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l - 1][c] < 0) plano[l - 1][c] = 0; } // B
-      if (c - 1 >= 0) { plano[l][c - 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l][c - 1] < 0) plano[l][c - 1] = 0; } // D
-      if (c + 1 < COLUNAS) { plano[l][c + 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l][c + 1] < 0) plano[l][c + 1] = 0; } // F
-      if (l + 1 < LINHAS) { plano[l + 1][c] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l + 1][c] < 0) plano[l + 1][c] = 0; } // H
-      if (l - 1 >= 0 && c - 1 >= 0) { plano[l - 1][c - 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l - 1][c - 1] < 0) [l - 1][c - 1] = 0; } // A
-      if (l - 1 >= 0 && c + 1 < COLUNAS) { plano[l - 1][c + 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l - 1][c + 1] < 0) plano[l - 1][c + 1] = 0; } // C
-      if (l + 1 < LINHAS && c - 1 >= 0) { plano[l + 1][c - 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l + 1][c - 1] < 0) plano[l + 1][c - 1] = 0; } // G
-      if (l + 1 < LINHAS && c + 1 < COLUNAS) { plano[l + 1][c + 1] += espaco * fatorDifusao * dt; qtdDifusoes++; if (plano[l + 1][c + 1] < 0) plano[l + 1][c + 1] = 0; } // I
-      plano[l][c] -= qtdDifusoes * fatorDifusao * dt;
-      if (plano[l][c] < 0) plano[l][c] = 0;
-      qtdDifusoes = 0;
-    }
-  }
-
+  return intensidadeRedor;
 }
 
-function decaimento(plano, dt) {
-  const fatorDecaimento = 0.25;
+function difusao(plano, planoFuturo, dt) {
+
+  const fatorDecaimento = 0.06;
+  const fatorDifusao = 10.5;
+
   for (let l = 0; l < LINHAS; l++) {
     for (let c = 0; c < COLUNAS; c++) {
-      if (plano[l][c] > 0) {
-        plano[l][c] += (-1 * fatorDecaimento * dt); // Diminui em 1 de intensidade por segundo
-        if (plano[l][c] < 0) {
-          plano[l][c] = 0;
-        }
-      }
+
+      const elemento = plano[l][c];
+      planoFuturo[l][c] = elemento + (fatorDifusao * calculaAcrescimoIntensidade(plano, l, c) - fatorDecaimento * elemento) * dt;
+      if (planoFuturo[l][c] < 0) { planoFuturo[l][c] = 0 };
+    }
+  }
+}
+
+function atualizaPlano(plano, planoFuturo) {
+  const aux = [];
+  for (let l = 0; l < LINHAS; l++) {
+    aux[l] = [];
+    for (let c = 0; c < COLUNAS; c++) {
+      aux[l][c] = plano[l][c];
+      plano[l][c] = planoFuturo[l][c];
+      planoFuturo[l][c] = aux[l][c];
     }
   }
 }
