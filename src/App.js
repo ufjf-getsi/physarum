3cc1b3baf83f929749992ef4f1c2551c4400d90e
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-06-14 04:56:39 -0300

    Aprimora CSS

--- src/App.js
+++ src/App.js
@@ -2,57 +2,67 @@ import React from 'react';
 import Canvas from './components/Canvas/Canvas';
 import './App.css';
 
-const LINHAS = 10;
-const COLUNAS = 15;
+// Tamanho da matriz de plano
+const LINHAS = 100;
+const COLUNAS = 100;
 
 export default function App() {
 
-  const matriz = [];
-  let t0 = null;
+  const plano = [];
+  let t0 = null; // Tempo inicial
 
+  // Preenche o plano com valores aleatórios
   for (let l = 0; l < LINHAS; l++) {
-    matriz[l] = [];
+    plano[l] = [];
     for (let c = 0; c < COLUNAS; c++) {
-      matriz[l][c] = Math.random() * 0;
+      plano[l][c] = Math.random() * 10; // Número aleatório entre 0 e 9.99...
     }
   }
 
-  matriz[5][2] = 10;
+  //Define um espaço no plano como valor máximo
+  plano[LINHAS / 2][COLUNAS / 2] = 10;
 
+  // Desenha na tela
   const draw = (ctx, t) => {
     if (t0 === null) { t0 = t; }
-    const dt = (t - t0) / 1000;
+    const dt = (t - t0) / 1000; // Intervalo entre tempo atual e anterior
 
+    // Limpa desenho anterior
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+    // Definição de valores no plano
     for (let l = 0; l < LINHAS; l++) {
       for (let c = 0; c < COLUNAS; c++) {
-        if (matriz[l][c] > 0) {
-          matriz[l][c] += (-1 * dt);
-        }
-        else {
-          matriz[l][c] = 0;
+        // Decaimento de intensidade
+        if (plano[l][c] > 0) {
+          plano[l][c] += (-1 * dt); // Diminui em 1 de intensidade por segundo
+          if (plano[l][c] < 0) {
+            plano[l][c] = 0;
+          }
         }
       }
     }
 
+    // Desenha na tela conforme os valores do plano
     for (let l = 0; l < LINHAS; l++) {
       for (let c = 0; c < COLUNAS; c++) {
-        ctx.fillStyle = `hsl(${(10 - matriz[l][c]) * 10}deg, 100%, 50%)`;
+        ctx.fillStyle = `hsl(${(10 - plano[l][c]) * 10}deg, 100%, 50%)`;
         ctx.fillRect(c * 10, l * 10, 10, 10);
+        //ctx.fillStyle = "black";
+        //ctx.fillText(plano[l][c].toFixed(0), l * 10, c * 10 + 10);
       }
     }
+    //ctx.fillText(dt * 1, 10, 10);
 
-    ctx.fillStyle = "black";
-    ctx.fillText(matriz[5][2], 50, 50);
-    ctx.fillText(dt * 1, 50, 60);
-
+    // Define tempo inicial da próxima animação como o tempo atual
     t0 = t;
   };
 
   return (
-    <div className="App">
-      <Canvas draw={draw} />
+    <div className="app">
+      <div className="app-container">
+        <Canvas draw={draw} width={LINHAS * 10} height={COLUNAS * 10} />
+      </div>
     </div>
   );
 }
--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect } from "react";
 import useCanvas from './useCanvas';
+import './canvas.css';
+
 
 export default function Canvas(props) {
 
-    const { draw, ...rest } = props;
+    const { draw, width, height, ...rest } = props;
     const canvasRef = useCanvas(draw);
 
-    return (<canvas ref={canvasRef} {...rest}></canvas>);
+    return (<canvas className="canvas" ref={canvasRef} width={width} height={height}></canvas>);
 }
